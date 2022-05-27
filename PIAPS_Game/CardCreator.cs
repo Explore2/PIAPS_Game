2a1a63f29f05b70d7681562b8c06ba2f7e1b4503
Leonid Fomichev <darkshift911@gmail.com>
2022-05-27 12:25:35 +0300

    Builder done

--- PIAPS_Game/CardCreator.cs
+++ PIAPS_Game/CardCreator.cs
@@ -3,16 +3,22 @@ using PIAPS_Game.Card;
 
 namespace PIAPS_Game;
 
-public class CardCreator_
+public class CardCreator
 {
     public Builder.Builder Builder;
 
     public AbstractCard CreateCard()
     {
-        return null;
+        Builder.SetHP();
+        Builder.SetDamage();
+        Builder.SetCost();
+        return Builder.GetCard();
     }
     public AbstractCard CreateEliteCard()
     {
-        return null;
+        Builder.SetEliteHP();
+        Builder.SetEliteDamage();
+        Builder.SetEliteCost();
+        return Builder.GetCard();
     }
 }
--- PIAPS_Game/Map/AbstractMap.cs
+++ PIAPS_Game/Map/AbstractMap.cs
@@ -1,8 +1,10 @@
 ﻿using PIAPS_Game.Card;
+using PIAPS_Game.Observer;
 
 namespace PIAPS_Game.Map;
 
-public class AbstractMap
+public abstract class AbstractMap : EventListener
 {
     public List<AbstractCard> Cards = new List<AbstractCard>();
+    public abstract void Update<TSender, TEventArgs>(TSender sender, TEventArgs eventArgs);
 }
